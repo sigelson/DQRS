f0d77f635dcd9c4282b4d9f4781768dd5f51faa2
Paul Adrian Katipunan <paul.katipunan13@gmail.com>
2021-04-17 03:44:06 +0800

    update

--- resources/views/layouts/appw.blade.php
+++ resources/views/layouts/appw.blade.php
@@ -9,13 +9,13 @@
 
         <title>{{ config('app.name', 'DQRS') }}</title>
         <!-- Favicon -->
-        <link href="{{ asset('/argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
+        <link href="{{ asset('assets/argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
         <script src="{{ asset('') }}/js/app.js"></script>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
         <!-- Icons -->
-        <link href="{{ asset('/argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-        <link href="{{ asset('/argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+        <link href="{{ asset('assets/argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+        <link href="{{ asset('assets/argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
         <!-- Argon CSS -->
         <link type="text/css" href="{{ asset('/argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
         <link type="text/css" href="{{ asset('/argon') }}/css/animate.css" rel="stylesheet">
@@ -37,13 +37,13 @@
 
 
 
-        <script src="{{ asset('/argon') }}/vendor/jquery/dist/jquery.min.js"></script>
-        <script src="{{ asset('/argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('assets/argon') }}/vendor/jquery/dist/jquery.min.js"></script>
+        <script src="{{ asset('assets/argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
         @stack('js')
 
 
         <!-- Argon JS -->
-        <script src="{{ asset('/argon') }}/js/argon.js?v=1.0.0"></script>
+        <script src="{{ asset('assets/argon') }}/js/argon.js?v=1.0.0"></script>
 
     </body>
 </html>
